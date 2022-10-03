86291ab52ceeb87ebda0d38cbe10367b9dadf1b1
David Pérez <dperezz@unal.edu.co>
2022-10-03 12:13:39 -0500

    I think it's complete

--- pruebas.py
+++ pruebas.py
@@ -73,4 +73,28 @@ def test_10():
         query = file.read()
     return pd.read_sql_query(query, conn)
 
-print(test_10())
+def test_11():
+    conn, _ = load_data()
+    with open("pregunta_11.sql", encoding="utf-8") as file:
+        query = file.read()
+    return pd.read_sql_query(query, conn)
+
+def test_12():
+    conn, _ = load_data()
+    with open("pregunta_12.sql", encoding="utf-8") as file:
+        query = file.read()
+    return pd.read_sql_query(query, conn)
+
+def test_13():
+    conn, _ = load_data()
+    with open("pregunta_13.sql", encoding="utf-8") as file:
+        query = file.read()
+    return pd.read_sql_query(query, conn)
+
+def test_14():
+    conn, _ = load_data()
+    with open("pregunta_14.sql", encoding="utf-8") as file:
+        query = file.read()
+    return pd.read_sql_query(query, conn)
+
+print(test_14())
